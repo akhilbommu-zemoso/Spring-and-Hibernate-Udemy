205fc4edea88a77c6d31e3954f0eda3dd3baa8b6
akhilbommu-zemoso <akhil.bommu@zemosolabs.com>
2021-07-19 22:43:58 +0530

    Spring Bean Scopes and Lifecycle

--- src/main/java/com/akhil/springdemo/TrackCoach.java
+++ src/main/java/com/akhil/springdemo/TrackCoach.java
@@ -21,4 +21,14 @@ public class TrackCoach implements Coach {
     public String getDailyFortune() {
         return "Just Do It: " + fortuneService.getFortune();
     }
+
+    // add an init method
+    public void doMyStartupStuff(){
+        System.out.println("TrackCoach: inside method doMyStartupStuff");
+    }
+
+    // add an destroy method
+    public void dpMyCleanupStuffYoYo(){
+        System.out.println("TrackCoach: inside method doMyCleanupStuffYoYo");
+    }
 }
